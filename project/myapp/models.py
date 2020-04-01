d3dc97f1501e9d4b5e439fd7dd4af04542631531
YaCpotato <yasshisshy@gmail.com>
2020-04-01 23:50:06 +0900

    model作成途中

--- project/myapp/models.py
+++ project/myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    JOB = (
+        (0, 'No job'),
+        (1, 'Fighter'),
+        (2, 'Magic Caster'),
+        (3, 'Priest')
+    )
+    name = models.CharField(max_length=8)
+    hp = models.IntegerField(null=False)
+    mp = models.IntegerField(null=False)
+    job = models.IntegerField(max_length=3,choices=JOB)
+    weapon = models.CharField(max_length=8)
+    pub_date = models.DateTimeField('date published')
