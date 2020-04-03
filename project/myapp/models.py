8fc579e45b9db7592250dc6b340fc3d8b608c21e
YaCpotato <yasshisshy@gmail.com>
2020-04-03 10:02:23 +0900

    モデル作成。MySQL登録

--- project/myapp/models.py
+++ project/myapp/models.py
@@ -1,16 +1,30 @@
 from django.db import models
 
 # Create your models here.
-class Player(models.Model):
-    JOB = (
-        (0, 'No job'),
-        (1, 'Fighter'),
-        (2, 'Magic Caster'),
-        (3, 'Priest')
+class Product(models.Model):
+    number = models.CharField(verbose_name='商品番号',max_length=10,primary_key=True)
+    name = models.CharField(verbose_name='商品名',max_length=200)
+    price = models.IntegerField(verbose_name='価格')
+
+class Client(models.Model):
+    number = models.CharField(verbose_name='顧客番号',max_length=10,primary_key=True)
+    name = models.CharField(verbose_name='顧客名',max_length=200)
+    adress = models.TextField(
+        verbose_name='住所',
+        max_length=1000
     )
-    name = models.CharField(max_length=8)
-    hp = models.IntegerField(null=False)
-    mp = models.IntegerField(null=False)
-    job = models.IntegerField(max_length=3,choices=JOB)
-    weapon = models.CharField(max_length=8)
-    pub_date = models.DateTimeField('date published')
+
+
+class Order(models.Model):
+    number = models.CharField(verbose_name='注文番号',max_length=10,primary_key=True)
+    client_number = models.ForeignKey(
+        'Client',
+        verbose_name='顧客',
+        on_delete=models.CASCADE
+    )
+    product_number = models.ForeignKey(
+        'Product',
+        verbose_name='商品',
+        on_delete=models.CASCADE
+    )
+    quantity = models.CharField(verbose_name='数量',max_length=200)
--- project/project/settings.py
+++ project/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'data_adventure_01',
+        'USER': 'da_tarou',
+        'PASSWORD': 'uorat_ad',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
